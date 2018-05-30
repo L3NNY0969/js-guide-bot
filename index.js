5369d010da9285e356afb57bfd06d664f8eaa6b3
L3NNY <35822194+L3NNY0969@users.noreply.github.com>
2018-05-30 18:59:42 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    client.user.setActivity('JS lessons', {type: 'WATCHING'});
+});
+
+client.on('message', msg => {
+    if (!msg.content.startsWith(process.env.PREFIX) || !msg.guild) return;
+    const command = msg.content.split(' ')[0].substr(process.env.PREFIX.length);
+    const args = msg.content.split(' ').slice(1).join(' ');
+    if (command === 'serverinvite') return msg.channel.send('https://discord.gg/FEPNu3A');
+    else if (command === 'invite') return msg.channel.send(n0pe);
+});
+
+client.login(process.env.TOKEN);
